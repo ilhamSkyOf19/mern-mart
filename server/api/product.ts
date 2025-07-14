981dc2342a90694296808f4ac2fe443bc14152aa
ilhamSkyOf19 <ilhamm778866@gmail.com>
2025-07-14 10:27:49 +0700

    deploy backend to vercel

--- server/api/product.ts
+++ server/api/product.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../application/db';
+import { prisma } from '../src/application/db';
 
 export default async function handler(
   req: NextApiRequest,
